fbde30eb5989bcc9dca5cc6221eb8b532622ebd9
jill_chou <jill_chou@gemteks.com>
2017-09-07 19:54:46 +0800

    add check wifi status

--- test_1_wifiSet.py
+++ test_1_wifiSet.py
@@ -14,49 +14,27 @@ PATH = lambda p: os.path.abspath(
 
 class wifi_test(unittest.TestCase):
     def _ScanSSID(self, ssid, button_id, start_y, end_y):
-
         scan_result = False
-
-        ssid_elem = None        
-
+        ssid_elem = None
         for i in range(10):
-
             elem = self.driver.find_elements_by_id(button_id)
-
             for e in elem:
-
                 if e.text == ssid:
-
                     scan_result = True
-
                     ssid_elem = e
-
                     break
-
             if scan_result:
-
                 break
-
             else:
-
                 self.driver.swipe(200, start_y , 200, end_y, 3000)
-
         ssid_elem.click()
     #initialize
     def setUp(self):
-        sleep(20)
-        
-        desired_caps={
-          'platformName': 'Android',
-          'platformVersion': '5.0',
-          'deviceName': 'E9AZCY15Z724',
-          'appPackage': 'com.android.settings',
-          'appActivity': 'Settings'
-        }
- 
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        sleep(2)
 
-        
+   #     sleep(50)
+
+
     #Cleaning up
     def tearDown(self):
         sleep(2)
@@ -64,6 +42,19 @@ class wifi_test(unittest.TestCase):
     
     #Test script
     def test_1_wifiSet(self):
+        os.system('adb -s F8AZCY230515 shell am start -n io.appium.settings/.Settings -e wifi on')
+        os.system('adb -s E9AZCY15Z724 shell am start -n io.appium.settings/.Settings -e wifi on')
+
+        desired_caps = {
+            'platformName': 'Android',
+            'platformVersion': '5.0',
+            'deviceName': 'E9AZCY15Z724',
+            'appPackage': 'com.android.settings',
+            'appActivity': 'Settings'
+        }
+
+        self.driver = webdriver.Remote('http://localhost:4729/wd/hub', desired_caps)
+
         elem = self.driver.find_elements_by_id("com.android.settings:id/title")
         for e in elem:
             try:
@@ -87,9 +78,61 @@ class wifi_test(unittest.TestCase):
                 e.click()
                 break
 
-    def test_2_wifi_closed(self):
-        os.system('adb shell am start -n io.appium.settings/.Settings -e wifi off')
-        
+    def test_2_wifiSet(self):
+
+        desired_caps = {
+            'platformName': 'Android',
+            'platformVersion': '6.0.1',
+            'deviceName': 'F8AZCY230515',
+            'appPackage': 'com.android.settings',
+            'appActivity': 'Settings'
+        }
+
+        self.driver = webdriver.Remote('http://localhost:4727/wd/hub', desired_caps)
+
+        elem = self.driver.find_elements_by_id("com.android.settings:id/title")
+        for e in elem:
+            try:
+                aaa = e.text.encode("GBK", 'ignore')
+            except Exception, c:
+                print c
+            m = re.match(r'Wi', aaa)
+            if m:
+                e.click()
+                sleep(2)
+                break
+
+        self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
+
+        elem = self.driver.find_elements_by_class_name('android.widget.TextView')
+        connect_status_fail = True
+        for e in elem:
+            if e.text == 'Connected':
+                connect_status_fail = False
+                print 'local already connect.'
+                break
+            else:
+                continue
+
+        if connect_status_fail:
+            element = self.driver.find_elements_by_class_name("android.widget.Button")
+            for e in element:
+                if e.text == 'Connect':
+                    e.click()
+
+            sleep(5)
+            self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
+            elem = self.driver.find_elements_by_class_name('android.widget.TextView')
+            connect_status = False
+            for e in elem:
+                if e.text == 'Connected':
+                    connect_status = True
+                    break
+                else:
+                    continue
+
+            assert connect_status,'Connect to local fail.'
+
 
 
 if __name__== '__main__' :
