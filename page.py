f06ec26777f702c551f0bf3d2bfe7f647ea748ef
jill_chou <jill_chou@gemteks.com>
2017-11-21 10:34:44 +0800

    Change to door ball

--- page.py
+++ page.py
@@ -1,6 +1,5 @@
 #coding=utf-8
 import time
-from element import BasePageElement
 from locators import *
 from time import sleep
 
@@ -62,11 +61,7 @@ def getText(self, element):
     except NoSuchElementException:
         return False
 
-class SearchTextElement(BasePageElement):
-    """This class gets the search text from the specified locator"""
 
-    #The locator for search box where search string is entered
-    locator = 'q'
 
 
 class BasePage(object):
@@ -76,20 +71,6 @@ class BasePage(object):
 
 
 
-class MainPage(BasePage):
-    """Home page action methods come here. I.e. Python.org"""
-
-    #Declares a variable that will contain the retrieved text
-    search_text_element = SearchTextElement()
-
-    def is_title_matches(self):
-        """Verifies that the hardcoded text "Python" appears in page title"""
-        return "Python" in self.driver.title
-
-    def click_go_button(self):
-        """Triggers the search"""
-        element = self.driver.find_element(*MainPageLocators.GO_BUTTON)
-        element.click()
 
 class SearchResultsPage(BasePage):
     """Search results page action methods come here"""
--- test_2_stressTest.py
+++ test_2_stressTest.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 import os
+import serial
 import unittest,HTMLTestRunner,time
 
 from selenium import webdriver
@@ -22,89 +23,143 @@ class LiteNetStressTest(unittest.TestCase):
         for f in files:
             if f[0:17]=='app-Litenet-debug':
                 apkname = f
-        #LiteNet
-##        desired_caps = {}
-##        desired_caps['app'] = PATH(apkname)
-##        desired_caps['appPackage'] = 'com.gemteks.litenet'
-##        desired_caps['appActivity'] = 'com.joymaster.mycasa.activity.MainActivity'
-##        desired_caps['platformName'] = 'Android'
-##        desired_caps['platformVersion'] = '4.4.3'
-##        desired_caps['deviceName'] = '015d321ff553f615'
-##        desired_caps['udid'] = '015d321ff553f615'
-##        self.driver = webdriver.Remote('http://localhost:4727/wd/hub', desired_caps)
-##        self.driver.implicitly_wait(35)
 
+
+        # LiteNet
         desired_caps = {}
         desired_caps['app'] = PATH(apkname)
         desired_caps['appPackage'] = 'com.gemteks.litenet'
         desired_caps['appActivity'] = 'com.joymaster.mycasa.activity.MainActivity'
         desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '6.0.1'
-        desired_caps['deviceName'] = 'F8AZCY230515'
-        desired_caps['udid'] = 'F8AZCY230515'
-        self.driver = webdriver.Remote('http://localhost:4727/wd/hub', desired_caps)
-        self.driver.implicitly_wait(35)       
+        desired_caps['platformVersion'] = '4.4.2'
+        desired_caps['deviceName'] = 'EAAZCY17E701'
+        desired_caps['udid'] = 'EAAZCY17E701'
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.driver.implicitly_wait(35)
 
-        
-        #Lincall
-        desired_caps = {}
-        desired_caps['appPackage'] = 'org.linphone'
-        desired_caps['appActivity'] = 'org.linphone.LinphoneLauncherActivity'
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '5.0'
-        desired_caps['deviceName'] = 'E9AZCY15Z724'
-        desired_caps['udid'] = 'E9AZCY15Z724'
-        self.LinDriver = webdriver.Remote('http://localhost:4729/wd/hub', desired_caps)
-        self.LinDriver.implicitly_wait(35)
+        # # Lincall
+        # desired_caps = {}
+        # desired_caps['appPackage'] = 'org.linphone'
+        # desired_caps['appActivity'] = 'org.linphone.LinphoneLauncherActivity'
+        # desired_caps['platformName'] = 'Android'
+        # desired_caps['platformVersion'] = '4.4.3'
+        # desired_caps['deviceName'] = '015d321ff553f615'
+        # desired_caps['udid'] = '015d321ff553f615'
+        #
+        # self.LinDriver = webdriver.Remote('http://localhost:4729/wd/hub', desired_caps)
+        # self.LinDriver.implicitly_wait(35)
 
-    def test_repeat_call(self): ##assert finished :D:D ##
+    def test_ring_call(self):
         welcome_page = page.WelcomePage(self.driver)
         login_page = page.LoginPage(self.driver)
         calling_page = page.CallingPage(self.driver)
         security_page = page.SecurityPage(self.driver)
 
         # LiteNet login
-        welcome_page.click_x_button() 
-        login_page.send_account_info('1@gmail.com', '12345678')
+        welcome_page.click_x_button()
+        login_page.send_account_info('jonathan_lin@gemteks.com', 'gemtektest123')
 
-        #lincall setting
-        lincall_welcome_page = page.LincallWellcomePage(self.LinDriver)
-        lincall_number_page = page.LincallNumberPage(self.LinDriver)
-        lincall_setting_page = page.LincallSettingPage(self.LinDriver)
-        lincall_calling_page = page.LincallCallingPage(self.LinDriver)
-        
-        lincall_welcome_page.click_num_button() 
-        lincall_number_page.click_set_button() 
-        lincall_setting_page.sound_set()
-        lincall_setting_page.debug_set()
-        lincall_setting_page.video_set()
-        lincall_setting_page.internet_set()
-        lincall_setting_page.click_num_button()
-        
-        lincall_number_page.enter_number('sip:192.168.11.1')
+        ser = serial.Serial("/dev/tty.usbserial", 9600, timeout=1)
+        if (ser.isOpen() == False):
+            ser.open()
+        ser.read()
+        ser.write("A")
+        sleep(1)
+        ser.write("a")
+        sleep(1)
+        ser.close()
+        if ser.is_open:
+            print 'open'
+        else:
+            print 'close'
+
         times = 500
         while times > 0:
-            lincall_number_page.call_out()
-            #LiteNet Receive call
 
-            if times == 500:
-                calling_page.allow_sound()
+            # LiteNet Receive call
 
-            assert calling_page.check_ring_page(), 'no ringing appear'            
-            calling_page.answer_call() 
-            assert calling_page.check_calling(),'current contact name not appear'
-            assert lincall_calling_page.check_calling(), 'current contact name not appear'
+            assert calling_page.check_ring_page(), 'no ringing appear'
+            calling_page.answer_call()
+            assert calling_page.check_calling(), 'current contact name not appear'
 
             calling_page.hangup()
             assert security_page.check_security_logo_appear(), 'Litenet hangup problem'
-            #lincall hangup
-            assert lincall_calling_page.check_hangup(), 'linphone hangup problem'
+            # lincall hangup
 
-            time1 = 500 - times +1
-            timestr = time.strftime('%Y/%m/%d %H:%M:%S',time.localtime(time.time()))
-            print "\n no. "+str(time1) +" call , time :"+str(timestr)
-            lincall_number_page.call_out()
+            time1 = 500 - times + 1
+            timestr = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time()))
+            print "\n no. " + str(time1) + " call , time :" + str(timestr)
             times = times - 1
+
+            ser.open()
+            if ser.is_open:
+                print 'open'
+            else:
+                print 'close'
+            ser.read()
+            ser.write("A")
+            sleep(1)
+            ser.write("a")
+            sleep(1)
+            ser.close()
+            if ser.is_open:
+                print 'oh no'
+            else:
+                print 'close'
+
+
+
+    #
+    #
+    #
+    # def test_repeat_call(self): ##assert finished :D:D ##
+    #     welcome_page = page.WelcomePage(self.driver)
+    #     login_page = page.LoginPage(self.driver)
+    #     calling_page = page.CallingPage(self.driver)
+    #     security_page = page.SecurityPage(self.driver)
+    #
+    #     # LiteNet login
+    #     welcome_page.click_x_button()
+    #     login_page.send_account_info('1@gmail.com', '12345678')
+    #
+    #     #lincall setting
+    #     lincall_welcome_page = page.LincallWellcomePage(self.LinDriver)
+    #     lincall_number_page = page.LincallNumberPage(self.LinDriver)
+    #     lincall_setting_page = page.LincallSettingPage(self.LinDriver)
+    #     lincall_calling_page = page.LincallCallingPage(self.LinDriver)
+    #
+    #     lincall_welcome_page.click_num_button()
+    #     lincall_number_page.click_set_button()
+    #     lincall_setting_page.sound_set()
+    #     lincall_setting_page.debug_set()
+    #     lincall_setting_page.video_set()
+    #     lincall_setting_page.internet_set()
+    #     lincall_setting_page.click_num_button()
+    #
+    #     lincall_number_page.enter_number('sip:192.168.11.1')
+    #     times = 500
+    #     while times > 0:
+    #         lincall_number_page.call_out()
+    #         #LiteNet Receive call
+    #
+    #         if times == 500:
+    #             calling_page.allow_sound()
+    #
+    #         assert calling_page.check_ring_page(), 'no ringing appear'
+    #         calling_page.answer_call()
+    #         assert calling_page.check_calling(),'current contact name not appear'
+    #         assert lincall_calling_page.check_calling(), 'current contact name not appear'
+    #
+    #         calling_page.hangup()
+    #         assert security_page.check_security_logo_appear(), 'Litenet hangup problem'
+    #         #lincall hangup
+    #         assert lincall_calling_page.check_hangup(), 'linphone hangup problem'
+    #
+    #         time1 = 500 - times +1
+    #         timestr = time.strftime('%Y/%m/%d %H:%M:%S',time.localtime(time.time()))
+    #         print "\n no. "+str(time1) +" call , time :"+str(timestr)
+    #         lincall_number_page.call_out()
+    #         times = times - 1
                
 
     def tearDown(self):
