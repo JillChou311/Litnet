57782e865d79ab26a087aebf30b632fa7c3edccc
jill_chou <jill_chou@gemteks.com>
2017-11-21 11:33:09 +0800

    Change wifi setting

--- test_1_wifiSet.py
+++ test_1_wifiSet.py
@@ -39,34 +39,28 @@ class wifi_test(unittest.TestCase):
     
     #Test script
     def test_1_wifiSet(self):
-        os.system('adb -s F8AZCY230515 shell am start -n io.appium.settings/.Settings -e wifi on')
-        os.system('adb -s E9AZCY15Z724 shell am start -n io.appium.settings/.Settings -e wifi on')
+        os.system('adb -s EAAZCY17E701 shell am start -n io.appium.settings/.Settings -e wifi on')
 
-        sleep(50)
+       # sleep(50)
 
         desired_caps = {
             'platformName': 'Android',
-            'platformVersion': '5.0',
-            'deviceName': 'E9AZCY15Z724',
+            'platformVersion': '4.4.2',
+            'deviceName': 'EAAZCY17E701',
             'appPackage': 'com.android.settings',
             'appActivity': 'Settings'
         }
 
-        self.driver = webdriver.Remote('http://localhost:4729/wd/hub', desired_caps)
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
-        elem = self.driver.find_elements_by_id("com.android.settings:id/title")
+        elem = self.driver.find_elements_by_id("android:id/title")
         for e in elem:
-            try:
-                aaa = e.text.encode("GBK", 'ignore')
-            except Exception, c:
-                print c
-            m = re.match(r'Wi', aaa)
-            if m:
+            if e.text == u"Wi‑Fi":
                 e.click()
                 sleep(2)
                 break
-            
-        self._ScanSSID("MyVITA_906E",'android:id/title',1000,200)
+
+        self._ScanSSID("MyVITA_A79F",'android:id/title',1000,200)
 
         elem = self.driver.find_elements_by_class_name("android.widget.Button")
         for e in elem:
@@ -77,60 +71,60 @@ class wifi_test(unittest.TestCase):
                 e.click()
                 break
 
-    def test_2_wifiSet(self):
-
-        desired_caps = {
-            'platformName': 'Android',
-            'platformVersion': '6.0.1',
-            'deviceName': 'F8AZCY230515',
-            'appPackage': 'com.android.settings',
-            'appActivity': 'Settings'
-        }
-
-        self.driver = webdriver.Remote('http://localhost:4727/wd/hub', desired_caps)
-
-        elem = self.driver.find_elements_by_id("com.android.settings:id/title")
-        for e in elem:
-            try:
-                aaa = e.text.encode("GBK", 'ignore')
-            except Exception, c:
-                print c
-            m = re.match(r'Wi', aaa)
-            if m:
-                e.click()
-                sleep(2)
-                break
-
-        self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
-
-        elem = self.driver.find_elements_by_class_name('android.widget.TextView')
-        connect_status_fail = True
-        for e in elem:
-            if e.text == 'Connected':
-                connect_status_fail = False
-                print 'local already connect.'
-                break
-            else:
-                continue
-
-        if connect_status_fail:
-            element = self.driver.find_elements_by_class_name("android.widget.Button")
-            for e in element:
-                if e.text == 'Connect':
-                    e.click()
-
-            sleep(5)
-            self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
-            elem = self.driver.find_elements_by_class_name('android.widget.TextView')
-            connect_status = False
-            for e in elem:
-                if e.text == 'Connected':
-                    connect_status = True
-                    break
-                else:
-                    continue
-
-            assert connect_status,'Connect to local fail.'
+    # def test_2_wifiSet(self):
+    #
+    #     desired_caps = {
+    #         'platformName': 'Android',
+    #         'platformVersion': '6.0.1',
+    #         'deviceName': 'F8AZCY230515',
+    #         'appPackage': 'com.android.settings',
+    #         'appActivity': 'Settings'
+    #     }
+    #
+    #     self.driver = webdriver.Remote('http://localhost:4727/wd/hub', desired_caps)
+    #
+    #     elem = self.driver.find_elements_by_id("com.android.settings:id/title")
+    #     for e in elem:
+    #         try:
+    #             aaa = e.text.encode("GBK", 'ignore')
+    #         except Exception, c:
+    #             print c
+    #         m = re.match(r'Wi', aaa)
+    #         if m:
+    #             e.click()
+    #             sleep(2)
+    #             break
+    #
+    #     self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
+    #
+    #     elem = self.driver.find_elements_by_class_name('android.widget.TextView')
+    #     connect_status_fail = True
+    #     for e in elem:
+    #         if e.text == 'Connected':
+    #             connect_status_fail = False
+    #             print 'local already connect.'
+    #             break
+    #         else:
+    #             continue
+    #
+    #     if connect_status_fail:
+    #         element = self.driver.find_elements_by_class_name("android.widget.Button")
+    #         for e in element:
+    #             if e.text == 'Connect':
+    #                 e.click()
+    #
+    #         sleep(5)
+    #         self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
+    #         elem = self.driver.find_elements_by_class_name('android.widget.TextView')
+    #         connect_status = False
+    #         for e in elem:
+    #             if e.text == 'Connected':
+    #                 connect_status = True
+    #                 break
+    #             else:
+    #                 continue
+    #
+    #         assert connect_status,'Connect to local fail.'
 
 
 
